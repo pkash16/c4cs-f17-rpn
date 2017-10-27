bc35249d1f0aaf87d49929884657b6c2acab7bf4
prakash_kumar <p.kash16@gmail.com>
2017-10-27 12:19:20 -0400

    Implemented ADD (+)

--- rpn.py
+++ rpn.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-	pass
-
-
+    stack = [] 
+    for item in arg.split():
+        if item == '+':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = (arg1) + (arg2)
+            stack.append(result)
+        else:
+            stack.append(int(item))
+    print(stack)
+    return stack.pop()
 def main():
 	while True:
 		calculate(input("rpn calc> "))
 
 if __name__ == '__main__':
 	main()
+
 
