3d446c2df5c7cba9cbb83e637fe5d34588faf583
prakash_kumar <p.kash16@gmail.com>
2017-10-30 17:59:26 -0400

    fixed spacing issues

--- test_rpn.py
+++ test_rpn.py
@@ -4,12 +4,12 @@ import rpn
 
 
 class TestBasics(unittest.TestCase):
-	def test_add(self):
-		result = rpn.calculate('1 1 +')
-		self.assertEqual(2, result)
-	def test_subtract(self):
-		result = rpn.calculate('5 2 -')
-		self.assertEqual(3, result)
-        def test_carat(self):
-            result = rpn.calculate('2 3 ^')
-            self.assertEqual(8, result)
+    def test_add(self):
+        result = rpn.calculate('1 1 +')
+        self.assertEqual(2, result)
+    def test_subtract(self):
+        result = rpn.calculate('5 2 -')
+        self.assertEqual(3, result)
+    def test_carat(self):
+        result = rpn.calculate('2 3 ^')
+        self.assertEqual(8, result)
